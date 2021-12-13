2eb83578def2eae0008c127bdbe25d116fb93aa0
Mohammad Javad Aghazadeh <af.mohammad.javad@gmail.com>
2021-12-13 03:51:47 +0330

    add marginTop to first div in ItemDND.

--- app/src/components/ItemDND.jsx
+++ app/src/components/ItemDND.jsx
@@ -3,7 +3,7 @@ import withDraggable from "../hoc/withDraggable";
 const ItemDND = ({ item }) => {
   return (
     <>
-      <div>&lt;{item.name}</div>
+      <div style={{marginTop: 35}}>&lt;{item.name}</div>
       <div>{item.age}&gt;</div>
     </>
   );
