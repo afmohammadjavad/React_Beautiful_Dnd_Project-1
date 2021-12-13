18eac62a18d16781eda46d50515d53977807bfe4
Mohammad Javad Aghazadeh <af.mohammad.javad@gmail.com>
2021-12-13 04:10:45 +0330

    use just margin and padding instead of flex becuase it was unnecessary.

--- app/src/components/Table.jsx
+++ app/src/components/Table.jsx
@@ -6,6 +6,9 @@ function Table() {
     { id: "1", name: "javad", age: '26' },
     { id: "2", name: "ali", age: '23' },
     { id: "3", name: "mohammad", age: '20' },
+    { id: "4", name: "hasan", age: '20' },
+    { id: "5", name: "arghun", age: '26' },
+    { id: "6", name: "reza", age: '26' },
   ]);
 
   return (
